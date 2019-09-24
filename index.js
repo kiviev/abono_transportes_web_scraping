d1cb4e40ae767a33f7fb3726999dd214da358782
Paco Alba <kiviev@gmail.com>
2019-09-24 19:22:10 +0200

    removido ejemplo

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 require('dotenv').config({ path: './.env' });
 const PuppetterPack = require('./PuppeteerPack');
-const Helpers = require('./Helpers');
-
-
-// sample use
-/* (async() =>{
-    let p = new PuppetterPack(process.env.ABONO_NUM);
-    await p.init().catch((res) => {
-        console.log('Init Fail: ', res);
-        process.exit(1);
-    });
-
-    let result = await p.getResults().catch((res) => console.log('Result Fail: ' , res));
-    console.log(result);
-
-    // console.log(Helpers.getAbonoResultToNotificate(result));
-
-    await p.close();
-
-})() */
 
 
 module.exports = PuppetterPack
